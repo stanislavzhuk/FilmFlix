df9db257708af022e826569f71764699a4bbd1fd
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-27 23:08:57 +0300

    update Movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,55 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import SearchBar from 'components/SearchBar/SearchBar';
 import Gallery from 'components/Gallery/Gallery';
 import GalleryItem from 'components/GalleryItem/GalleryItem';
 import { searchMovies } from 'services/themoviedb-api';
 
 const Movies = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [movies, setMovies] = useState([]);
   const location = useLocation();
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const value = searchParams.get('query') ?? '';
 
   useEffect(() => {
     const getSearchMoviesData = async () => {
       try {
-        const { results } = await searchMovies(searchQuery);
+        const { results } = await searchMovies(value);
         setMovies(results);
         // console.log(results);
       } catch (error) {
         console.error(error.message);
       }
     };
-    
-    if (searchQuery !== '') {
-      getSearchMoviesData();
-    } else {
-      setMovies([]);
-    }
-  }, [searchQuery]);
-  
+
+    getSearchMoviesData();
+  }, [value]);
+
   const handleSearchChange = (value) => {
-    setSearchQuery(value);
+    setSearchParams({ query: value });
   };
-  
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(searchQuery);
+    setSearchParams({ query: value });
   };
-  
+
   return (
     <div>
       <form>
         <button onClick={handleSearchSubmit}>Search</button>
-        <SearchBar value={searchQuery} onChange={handleSearchChange} />
+        <SearchBar value={value} onChange={handleSearchChange} />
       </form>
 
-      {movies.length === 0 && searchQuery && (
-        <h2>No results found</h2>
-      )}
+      {movies.length === 0 && value && <h2>No results found</h2>}
 
       <Gallery>
-        {movies.map(movie => (
+        {movies.map((movie) => (
           <GalleryItem data={movie} key={movie.id} state={{ from: location }} />
         ))}
       </Gallery>
