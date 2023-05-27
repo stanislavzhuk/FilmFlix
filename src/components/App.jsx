f5ae2db3fd5866512faffd343d0e7e9f76c99efe
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-27 12:48:42 +0300

    add react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
+import React from 'react'
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
+
+export default App
