fdc788e5d71223b25d599c031cc7daeadb235dcc
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-28 22:50:03 +0300

    add defaultImages

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import defaultPosterImage from '../../images/default_poster.png';
 
 const GalleryItem = ({ data, state }) => {
   const { id, poster_path, release_date, title, vote_average } = data;
+  const posterImage = poster_path ? `https://image.tmdb.org/t/p/w200${poster_path}` : defaultPosterImage;
+
   return (
     <li>
       <Link to={`/movies/${id}`} state={state}>
@@ -13,7 +16,7 @@ const GalleryItem = ({ data, state }) => {
             <li>{release_date}</li>
           </ul>
         </div>
-        <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} loading="lazy" width="180" />
+        <img src={posterImage} alt={title} loading="lazy" width="180" />
       </Link>
     </li>
   )
