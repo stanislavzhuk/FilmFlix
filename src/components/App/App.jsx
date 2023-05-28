2dc7b13cf3c588dd98e34d8e07b584063bdea3b7
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-28 17:20:17 +0300

    add Layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Layout from 'components/Layout/Layout';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
@@ -7,21 +8,16 @@ import MovieDetails from 'pages/MovieDetails';
 const App = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <NavLink to='./'>Home</NavLink>
-          <NavLink to='./movies'>Movies</NavLink>
-        </nav>
-      </header>
-
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/movies/:movieId' element={<MovieDetails />}>
-          <Route path='cast' />
-          <Route path='reviews' />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='movies' element={<Movies />} />
+          <Route path='movies/:movieId' element={<MovieDetails />}>
+            <Route path='cast' />
+            <Route path='reviews' />
+          </Route>
+          <Route path='*' />
         </Route>
-        <Route path='*' />
       </Routes>
     </div>
   );
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+          <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+          <NavLink to='/movies'>Movies</NavLink>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+};
+
+export default Layout;
